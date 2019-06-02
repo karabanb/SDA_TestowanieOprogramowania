f5d541102dee4e2413347e64c212efa19b22c681
karabanb <bkarabands@gmail.com>
2019-06-02 10:55:23 +0200

    Unitest2.py was added

---
+++ test_unitest2.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class SecondTest(unittest.TestCase):
+
+    def test_01_succes(self):
+        print("01")
+        self.assertEqual(2, 2)
+
+    def test_02_failure(self):
+        print("02")
+        self.assertEqual(2, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
